712abaf5557897ff1b5dbfe3429686a52e2fc305
Phungchau711 <ngocchauphung88@gmail.com>
2026-03-09 13:53:38 +0700

    thêm phần vé gửi

--- dbConfig.js
+++ dbConfig.js
@@ -1,7 +1,7 @@
 const config = {
 
     //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=localhost\\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;',
-   connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-6D4G1B3\\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;',
+   connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=localhost\\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;',
 }
 
 
