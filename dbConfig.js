1555e31a2439d36bc5b6eb9415e7c118837cd33c
hmy2704 <Hmy27042004@gmail.com>
2026-03-11 20:01:03 +0700

    sửa lỗi kết nối

--- dbConfig.js
+++ dbConfig.js
@@ -1,15 +1,15 @@
 const config = {
     // Nên dùng localhost để linh hoạt hơn nếu chạy trên máy cá nhân
-    connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=localhost\\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;'
-};
+    //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=localhost\\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;'
+
 
     //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=localhost\\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;',
-   connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=localhost\\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;',
+    //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=localhost\\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;',
     //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=DELL-PC;Database=QuanLyBaiXe;Trusted_Connection=yes;',
-connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=Ham;Database=QuanLyBaiXe;Trusted_Connection=yes;',
+    //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=Ham;Database=QuanLyBaiXe;Trusted_Connection=yes;',
 
-    connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=ADMIN-PC\\SQLEXPRESS01;Database=QuanLyBaiXe;Trusted_Connection=yes;',
-}
+    connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=192.168.32.233\\SQLEXPRESS01;Database=QuanLyBaiXe;Trusted_Connection=yes;',
+};
 
 
 
