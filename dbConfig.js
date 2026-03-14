70089488534498e53cd839b1f5a29151d2e2a2a1
hmy2704 <Hmy27042004@gmail.com>
2026-03-14 14:58:43 +0700

    cập nhật database

--- dbConfig.js
+++ dbConfig.js
@@ -1,41 +1,13 @@
 const config = {
 
-    // Dùng localhost và double backslash (\\) để tránh lỗi format
-<<<<<<< HEAD
-<<<<<<< HEAD
-    connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-6D4G1B3\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;'
-};
-=======
-=======
-    connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-6D4G1B3\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;'
-};
->>>>>>> 680191bb37fcecb6bd0fd9fe67b1f1a827df92b5
-    //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-6D4G1B3\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;'
 
 
-    // Nên dùng localhost để linh hoạt hơn nếu chạy trên máy cá nhân
-    //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=localhost\\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;'
 
-
-    //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=localhost\\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;',
-
-    //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=localhost\\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;',
-    //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=DELL-PC;Database=QuanLyBaiXe;Trusted_Connection=yes;',
-    //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=Ham;Database=QuanLyBaiXe;Trusted_Connection=yes;',
-
-    //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=localhost\\SQLEXPRESS;Database=QuanLyBaiXe;Trusted_Connection=yes;',
-    //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=DELL-PC;Database=QuanLyBaiXe;Trusted_Connection=yes;',
-    //connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=Ham;Database=QuanLyBaiXe;Trusted_Connection=yes;',
-
-    //
     connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=ADMIN-PC\\SQLEXPRESS01;Database=QuanLyBaiXe;Trusted_Connection=yes;',
 };
 
 
 
-<<<<<<< HEAD
->>>>>>> fec2f10f071b73c7ad2483773a8949483bfc9547
-=======
->>>>>>> 680191bb37fcecb6bd0fd9fe67b1f1a827df92b5
+
 
 module.exports = config;
